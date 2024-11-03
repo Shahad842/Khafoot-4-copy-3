e4444444552067a1b692888310856c68ca1d497a
Shahad842 <shahad_9215@hotmail.com>
2024-11-03 12:40:43 +0300

    Initial Commit

---
+++ Khafoot/KhafootApp.swift
@@ -0,0 +1,17 @@
+//
+//  KhafootApp.swift
+//  Khafoot
+//
+//  Created by Shahad Alhothali on 01/05/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct KhafootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
